b72bfb39d09e863d51a4d8d29cf360cdea5d1e65
Peter Rausch <rauschp123@gmail.com>
2023-02-14 20:38:32 -0600

    Pushing because stuff

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"net"
 
 	"github.com/rauschp/nexis-chain/crypto"
+	"github.com/rauschp/nexis-chain/node"
 )
 
 func main() {
@@ -12,4 +14,18 @@ func main() {
 
 	fmt.Println(hex.EncodeToString(privateKey.Key))
 	fmt.Println(hex.EncodeToString(privateKey.Public().Key))
+}
+
+func createServerNode(addr string) (*node.ServerNode, error) {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	node := &node.ServerNode{
+		HostAddress: addr,
+		Listener:    listener,
+	}
+
+	return node, nil
 }
