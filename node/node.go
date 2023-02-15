b9f03c146d445eb17c8ffb8abacb9d669ac2df86
Peter Rausch <rauschp123@gmail.com>
2023-02-14 20:41:37 -0600

    Fix protos

--- node/node.go
+++ node/node.go
@@ -8,17 +8,3 @@ type ServerNode struct {
 	HostAddress string
 	Listener    net.Listener
 }
-
-func New(addr string) (*ServerNode, error) {
-	listener, err := net.Listen("tcp", addr)
-	if err != nil {
-		return nil, err
-	}
-
-	node := &ServerNode{
-		HostAddress: addr,
-		Listener:    listener,
-	}
-
-	return node, nil
-}
